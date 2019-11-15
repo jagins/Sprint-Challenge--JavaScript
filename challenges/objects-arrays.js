f83bbbfc9fe92f36d18cc23e6d97c56cd097c501
jagins <jagins629@gmail.com>
2019-11-15 12:24:40 -0600

    Forgot to do unisWithUni done and passed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -106,7 +106,14 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
-//console.log(unisWithUni);
+graduates.forEach(function(name)
+{
+  if(name.university.includes('Uni'))
+  {
+    unisWithUni.push(name.university);
+  }
+});
+console.log(unisWithUni);
 
 
 // ==== ADVANCED Array Methods ====
